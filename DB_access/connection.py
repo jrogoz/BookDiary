d37c8fd7cedf1199c6ad6dce5bc6f989dc68ccd0
jrogoz <joanna.rogoz33@gmail.com>
2023-12-29 16:13:49 +0100

    Improve connection to the database

---
+++ DB_access/connection.py
@@ -0,0 +1,35 @@
+import psycopg2
+from config import config
+
+
+def connect():
+    connection = None
+    cursor = None
+
+    try:
+        params = config()
+        print('Connecting to the postgreSQL database ...')
+        connection = psycopg2.connect(**params)
+
+        cursor = connection.cursor()
+        print('PostgreSQL database version: ')
+        cursor.execute('SELECT version()')
+        db_version = cursor.fetchone()
+        print(db_version)
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        return connection, cursor
+
+
+def disconnect(connection, cursor):
+
+    try:
+        cursor.close()
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+            print('Database connection terminated.')
--- create_db.py
+++ create_db.py
@@ -1,27 +1,6 @@
-import psycopg2
-from config import config
-
-
-def connect():
-    connection = None
-    try:
-        params = config()
-        print('Connecting to the postgreSQL database ...')
-        connection = psycopg2.connect(**params)
-
-        cursor = connection.cursor()
-        print('PostgreSQL database version: ')
-        cursor.execute('SELECT version()')
-        db_version = cursor.fetchone()
-        print(db_version)
-        cursor.close()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if connection is not None:
-            connection.close()
-            print('Database connection terminated.')
-
+from DB_access.connection import connect, disconnect
 
 if __name__ == "__main__":
-    connect()
+    conn, cur = connect()
+    disconnect(conn, cur)
+
