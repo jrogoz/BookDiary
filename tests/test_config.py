74391429ed572167147d84795014d4391a12e36b
jrogoz <joanna.rogoz33@gmail.com>
2023-12-31 17:17:46 +0100

    Add config.py tests

---
+++ tests/test_config.py
@@ -0,0 +1,27 @@
+import unittest
+from config import config
+
+
+class TestConfig(unittest.TestCase):
+    def test_config_correct_arguments(self):
+        expected_db = {
+            'host': 'localhost',
+            'port': '5433',
+            'database': 'BookDiaryDB',
+            'user': 'postgres',
+            'password': '1234'
+        }
+        db = config(filename='test_database.ini')
+        self.assertDictEqual(expected_db, db)
+
+    def test_config_incorrect_filename(self):
+        with self.assertRaises(OSError):
+            config(filename='test.ini')
+
+    def test_config_incorrect_section(self):
+        with self.assertRaises(Exception):
+            config(section='notpostgresql')
+
+
+if __name__ == '__main__':
+    unittest.main()
