c1e5395e8ccf59f9242538e01c1bd13cf8309586
jrogoz <joanna.rogoz33@gmail.com>
2024-01-02 16:46:20 +0100

    Add modify_db.py tests

---
+++ tests/test_DB_access/test_modify_db.py
@@ -0,0 +1,21 @@
+import unittest
+import psycopg2
+from DB_access.modify_db import exec_query
+
+
+class TestModifyDB(unittest.TestCase):
+    def test_exec_query_OK_not_select(self):
+        query = "SELECT * FROM books"
+        result = exec_query(query)
+        self.assertEqual(None, result)
+
+    def test_exec_query_OK_select(self):
+        query = "SELECT * FROM books"
+        result = exec_query(query, select=1)
+        self.assertNotEqual(None, result)
+
+    def test_exec_query_not_string(self):
+        query = 5
+        exec_query(query)
+        self.assertRaises(psycopg2.DatabaseError)
+
